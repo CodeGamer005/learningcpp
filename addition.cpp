9b2af16200a6f0f3a2e4069c5d114624f8ff6bec
CodeGamer005 <134540938+CodeGamer005@users.noreply.github.com>
2023-05-31 00:13:27 +0530

    addition

---
+++ addition.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	double x, y, ans;
+	cout << "enter number: ";
+	cin >> x;
+	
+	cout << "enter 2nd number: ";
+	cin >> y;
+	
+	ans = x + y;
+	cout << "ans: " << ans;
+	return 0;
+}
